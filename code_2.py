00320d7c5ef2ea5ef64770e8ae5b48c6fb37e437
Геннадий <140153539+tangogenlu@users.noreply.github.com>
2023-12-01 19:02:39 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+        for (int item : data) {
+    }
+
+}
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+            System.out.println("Random Number: " + item);
+        }
+public class RandomDataGenerator {
+    public static void main(String[] args) {
